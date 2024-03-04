409ee8ce7ee2d4d5ca38d0d018c104937b385968
itsme1502 <harshitshankrit@gmail.com>
2024-03-04 20:35:25 +0530

    Course Creation Started

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,13 +1,82 @@
-const CourseIdPage = ({
+import { IconBadge } from "@/components/icon-badge";
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { LayoutDashboard } from "lucide-react";
+import { redirect } from "next/navigation";
+import toast from "react-hot-toast";
+import { TitleForm } from "./_components/title-form";
+
+
+
+const CourseIdPage = async ({
     params
 }:{
     params : {courseId : string}
 }
 ) => {
+
+    const {userId} = auth();
+
+    
+
+    if(!userId){
+        // toast.error("User Doesn't Exist");
+        return redirect('/');
+    }
+
+    const course = await db.course.findUnique({
+      where: {
+        id: params.courseId,
+      },
+    });
+
+    if(!course){
+        // toast.error("Course Doesn't Exist");
+        return redirect("/");
+    }
+
+    const requiredFields = [
+        course.title,
+        course.description,
+        course.imageUrl,
+        course.categoryId,
+        course.price
+    ]
+
+    const totalFields = requiredFields.length;
+    const completedFields = requiredFields.filter(Boolean).length;
+
+    const completionText = `(${completedFields}/${totalFields})`;
+
     return ( 
         
-        <div>
-            Course Id : {params.courseId}
+        <div className="p-6">
+            <div className="flex items-center justify-between">
+                <div className="flex flex-col gap-y-2">
+                   <h1 className="text-2xl font-medium">
+                    Course Setup
+                   </h1>
+                   <span className="text-sm  text-slate-700">
+                    Complete all Fields {completionText}
+                   </span>
+                </div>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+                <div>
+                    <div className="flex items-center gap-x-2">
+                        <IconBadge icon = {LayoutDashboard}/>
+                     <h2 className="text-xl">
+                        Customize Your Course
+                     </h2>
+                    </div>
+                   <TitleForm 
+                   initialData = {course}
+                   courseId = {course.id}
+                    />
+
+                </div>
+            </div>
+
         </div>
      );
 }
