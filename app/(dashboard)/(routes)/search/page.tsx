8c81fbe88b3a884398024c7e1c7e5f7c15242786
itsme1502 <harshitshankrit@gmail.com>
2024-03-22 16:50:21 +0530

    Searching Course Initiaited

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,7 +1,20 @@
-const SearchPage = () => {
+import { db } from "@/lib/db";
+import { Categories } from "./_components/categories";
+
+const SearchPage = async () => {
+
+    const categories = await db.category.findMany({
+        orderBy : {
+            name : "asc",
+        }
+    });
+
+
+
+
     return ( 
-        <div>
-            Search
+        <div className="p-6">
+           <Categories items={categories} />
         </div>
      );
 }
