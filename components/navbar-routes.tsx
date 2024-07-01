d492026bd01f491a32164bcc32b43e781d9d3b03
itsme1502 <harshitshankrit@gmail.com>
2024-07-01 21:27:53 +0530

    Added Sign-In button

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -2,7 +2,7 @@
 
 import { UserButton, useAuth } from "@clerk/nextjs";
 import { usePathname } from "next/navigation";
-import { LogOut } from "lucide-react";
+import { LogIn, LogOut } from "lucide-react";
 import Link from "next/link";
 
 import { Button } from "@/components/ui/button";
@@ -14,35 +14,49 @@ export const NavbarRoutes = () => {
   const { userId } = useAuth();
   const pathname = usePathname();
 
-  
   const isTeacherPage = pathname?.startsWith("/teacher");
   const isCoursePage = pathname?.includes("/courses");
   const isSearchPage = pathname === "/search";
 
   return (
     <>
-      {isSearchPage && (
-        <div className="hidden md:block">
-          <SearchInput />
-        </div>
+      {!userId ? (
+        <>
+          <div className="flex gap-x-2 ml-auto">
+            <Link href="/sign-in">
+              <Button size="sm">
+                <LogIn className="h-4 w-4 mr-2" />
+                Sign In
+              </Button>
+            </Link>
+          </div>
+        </>
+      ) : (
+        <>
+          {isSearchPage && (
+            <div className="hidden md:block">
+              <SearchInput />
+            </div>
+          )}
+          <div className="flex gap-x-2 ml-auto">
+            {isTeacherPage || isCoursePage ? (
+              <Link href="/">
+                <Button size="sm" variant="ghost">
+                  <LogOut className="h-4 w-4 mr-2" />
+                  Exit
+                </Button>
+              </Link>
+            ) : isTeacher(userId) ? (
+              <Link href="/teacher/courses">
+                <Button size="sm" variant="ghost">
+                  Teacher mode
+                </Button>
+              </Link>
+            ) : null}
+            <UserButton afterSignOutUrl="/" />
+          </div>
+        </>
       )}
-      <div className="flex gap-x-2 ml-auto">
-        {isTeacherPage || isCoursePage ? (
-          <Link href="/">
-            <Button size="sm" variant="ghost">
-              <LogOut className="h-4 w-4 mr-2" />
-              Exit
-            </Button>
-          </Link>
-        ) : isTeacher(userId) ? (
-          <Link href="/teacher/courses">
-            <Button size="sm" variant="ghost">
-              Teacher mode
-            </Button>
-          </Link>
-        ) : null}
-        <UserButton afterSignOutUrl="/" />
-      </div>
     </>
   );
 };
